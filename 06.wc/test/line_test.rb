544f281d666ad7487676d45fdf99efdae2f46134
github Kassy <github@shinbokutaro0220@gmail.com>
2022-06-30 15:14:09 +0900

    引数に空のファイルを指定した場合のテストを追加しました

--- 06.wc/test/line_test.rb
+++ 06.wc/test/line_test.rb
@@ -15,6 +15,12 @@ class LineTest < Minitest::Test
     assert_equal expected, main([first_target_file_path, second_target_file_path])
   end
 
+  def test_empty_file
+    target_file_path = 'test/fixtures/empty.txt'
+    expected = `wc -l #{target_file_path}`.chomp
+    assert_equal expected, main([target_file_path])
+  end
+
   def test_stdin_lines
 
   end
