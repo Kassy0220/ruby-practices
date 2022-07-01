e8c3c0b2735533d10599a4b583a778922a24adf2
github Kassy <github@shinbokutaro0220@gmail.com>
2022-07-01 09:30:34 +0900

    テストを assert_output に変更しました

--- 06.wc/test/line_test.rb
+++ 06.wc/test/line_test.rb
@@ -1,27 +1,25 @@
+# frozen_string_literal: true
+
 require 'minitest/autorun'
 require_relative '../lib/wc'
 
 class LineTest < Minitest::Test
   def test_a_file_lines
     target_file_path = 'test/fixtures/sample.txt'
-    expected = `wc -l #{target_file_path}`.chomp
-    assert_equal expected, main([target_file_path])
+    expected = `wc -l #{target_file_path}`
+    assert_output(expected) { puts main([target_file_path]) }
   end
 
   def test_two_files_lines
     first_target_file_path = 'test/fixtures/sample.txt'
     second_target_file_path = 'test/fixtures/second_sample.txt'
-    expected = `wc -l #{first_target_file_path} #{second_target_file_path}`.chomp
-    assert_equal expected, main([first_target_file_path, second_target_file_path])
+    expected = `wc -l #{first_target_file_path} #{second_target_file_path}`
+    assert_output(expected) { puts main([first_target_file_path, second_target_file_path]) }
   end
 
   def test_empty_file
     target_file_path = 'test/fixtures/empty.txt'
-    expected = `wc -l #{target_file_path}`.chomp
-    assert_equal expected, main([target_file_path])
-  end
-
-  def test_stdin_lines
-
+    expected = `wc -l #{target_file_path}`
+    assert_output(expected) { puts main([target_file_path]) }
   end
 end
