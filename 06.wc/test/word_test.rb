ff8ed3eff75de68d0385e504baac3ffd6cb2234f
github Kassy <github@shinbokutaro0220@gmail.com>
2022-07-03 07:27:37 +0900

    -wオプションのテスト(空のファイルの場合)を作成しました

--- 06.wc/test/word_test.rb
+++ 06.wc/test/word_test.rb
@@ -20,6 +20,13 @@ class WordTest < Minitest::Test
     assert_output(expected) { puts count_files([first_target_file_path, second_target_file_path], params) }
   end
 
+  def test_count_empty_file
+    target_file_path = 'test/fixtures/empty.txt'
+    expected = `wc -w #{target_file_path}`
+    params = { count_word: true }
+    assert_output(expected) { puts count_files([target_file_path], params) }
+  end
+
   def test_count_stdin_words
     text = 'RuboCop is a Ruby static code analyzer (a.k.a. linter) and code formatter. Out of the box it will enforce many of the guidelines outlined in the community Ruby Style Guide.'
     stdin = "<<EOS
