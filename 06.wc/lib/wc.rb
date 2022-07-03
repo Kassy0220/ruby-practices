76d5610ce3c327452ed5f6ce9c7b0bccabc7335a
github Kassy <github@shinbokutaro0220@gmail.com>
2022-07-04 07:33:20 +0900

    -cオプション 引数がファイルの場合の処理を作成しました

--- 06.wc/lib/wc.rb
+++ 06.wc/lib/wc.rb
@@ -19,22 +19,24 @@ def count_files(argv, params)
     file[:name] = target_file
     file[:line] = count_line(text) if params[:count_line]
     file[:word] = count_word(text) if params[:count_word]
+    file[:byte] = count_byte(text) if params[:count_byte]
 
     counting_results.push file
   end
   
   if counting_results.size >= 2
     total = {}
+    ## TODO: メソッドに切り出す??
     total[:name] = 'total'
-    ## TODO: メソッドに切り出す
     total[:line] = counting_results.sum { |file| file[:line] } if params[:count_line]
     total[:word] = counting_results.sum { |file| file[:word] } if params[:count_word]
+    total[:byte] = counting_results.sum { |file| file[:byte] } if params[:count_byte]
     counting_results.push total
   end
 
   format_model = create_format(params)
   counting_results.map do |file|
-    format(format_model, line: file[:line], word: file[:word], name: file[:name])
+    format(format_model, line: file[:line], word: file[:word], byte: file[:byte], name: file[:name])
   end.join("\n")
 end
 
@@ -46,10 +48,15 @@ def count_word(text)
   text.scan(/\S+/).size
 end
 
+def count_byte(text)
+  text.chars.map(&:bytesize).sum
+end
+
 def create_format(params)
   format_model = []
   format_model.push '%<line>8d' if params[:count_line]
   format_model.push '%<word>8d' if params[:count_word]
+  format_model.push '%<byte>8d' if params[:count_byte]
   format_model.push ' %<name>s' 
   format_model.join
 end
