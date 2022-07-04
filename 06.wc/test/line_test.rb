94c6ca62c06b826e52810b6a03e215f9ff2f3aff
github Kassy <github@shinbokutaro0220@gmail.com>
2022-07-04 13:57:15 +0900

    重複箇所を無くしました(line_test.rb)

--- 06.wc/test/line_test.rb
+++ 06.wc/test/line_test.rb
@@ -5,37 +5,38 @@ require 'stringio'
 require_relative '../lib/wc'
 
 class LineTest < Minitest::Test
-  def test_count_lines_in_a_file
-    target_file_path = 'test/fixtures/sample.txt'
-    expected = `wc -l #{target_file_path}`
-    params = { count_line: true }
-    assert_output(expected) { puts count_files([target_file_path], params) }
-  end
-
-  def test_count_lines_in_two_files
-    first_target_file_path = 'test/fixtures/sample.txt'
-    second_target_file_path = 'test/fixtures/second_sample.txt'
-    expected = `wc -l #{first_target_file_path} #{second_target_file_path}`
-    params = { count_line: true }
-    assert_output(expected) { puts count_files([first_target_file_path, second_target_file_path], params) }
-  end
-
-  def test_count_lines_in_empty_file
-    target_file_path = 'test/fixtures/empty.txt'
-    expected = `wc -l #{target_file_path}`
-    params = { count_line: true }
-    assert_output(expected) { puts count_files([target_file_path], params) }
-  end
-
-  def test_count_lines_in_stdin
-    stdin = "<<EOS
+  TARGET_PATH = 'test/fixtures/sample.txt'
+  SECOND_TARGET_PATH = 'test/fixtures/second_sample.txt'
+  EMPTY_TARGET_PATH = 'test/fixtures/empty.txt'
+  HEREDOCUMENT = "<<EOS
 hoge
 fuga
 piyo
 EOS\n"
-    expected = `wc -l #{stdin}`
-    $stdin = StringIO.new("hoge\nfuga\npiyo\n")
+  TEXT = "hoge\nfuga\npiyo\n"
+
+  def test_count_lines_in_a_file
+    expected = `wc -l #{TARGET_PATH}`
+    params = { count_line: true }
+    assert_output(expected) { puts count_files([TARGET_PATH], params) }
+  end
+
+  def test_count_lines_in_two_files
+    expected = `wc -l #{TARGET_PATH} #{SECOND_TARGET_PATH}`
+    params = { count_line: true }
+    assert_output(expected) { puts count_files([TARGET_PATH, SECOND_TARGET_PATH], params) }
+  end
+
+  def test_count_lines_in_empty_file
+    expected = `wc -l #{EMPTY_TARGET_PATH}`
+    params = { count_line: true }
+    assert_output(expected) { puts count_files([EMPTY_TARGET_PATH], params) }
+  end
+
+  def test_count_lines_in_stdin
+    expected = `wc -l #{HEREDOCUMENT}`
+    $stdin = StringIO.new(TEXT)
     params = { count_line: true }
     assert_output(expected) { puts count_stdin(params) }
-  end 
+  end
 end
